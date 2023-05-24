c90e8f6eeb12be6fbaeb2b2d3e4b3e7a86a646ad
Aime Victory <victorydarnelmbenze@gmail.com>
2023-05-24 11:09:25 +0200

    "options next config"

--- next.config.js
+++ next.config.js
@@ -1,20 +1,4 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
+const nextConfig = {}
 
-module.exports = {
-  trailingSlash: true,
-  reactStrictMode: false,
-  experimental: {
-    esmExternals: false,
-    jsconfigPaths: true // enables it for both jsconfig.json and tsconfig.json
-  },
-  webpack: config => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
-    }
-
-    return config
-  }
-}
-
+module.exports = nextConfig
