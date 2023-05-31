47e8f4a817f09e88bd40eab5c151491365128ac5
booteur <berthonge.mibambou.tongo@horizon-tech.com>
2023-05-31 15:01:49 +0100

    version 1

--- app/data/link.ts
+++
@@ -1,9 +0,0 @@
-/*
-export const navLink = [
-  { id: 1, url: "/", link: "Home" },
-  { id: 2, url: "/about", link: "About" },
-  { id: 3, url: "/models", link: "Vehicle Models" },
-  { id: 5, url: "/team", link: "Our Team" },
-  { id: 6, url: "/contact", link: "Contact" },
-];
-*/
